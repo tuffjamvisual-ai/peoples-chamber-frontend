7894a87a3a56bd0c54e656a4d467f92f5fcd18c5
Your Name <tuffjamvisual@gmail.com>
2026-04-09 23:10:37 +0100

    Fix Navigation import path in agency page

--- app/agencies/[slug]/page.tsx
+++ app/agencies/[slug]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, use, useEffect } from 'react';
-import Navigation from '../../../components/Navigation';
+import Navigation from '../../components/Navigation';
 import Link from 'next/link';
 
 type AgencyData = {
