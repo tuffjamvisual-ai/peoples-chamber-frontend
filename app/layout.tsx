de2bcbf9d90228e24970c19861a11bcbf3c48526
Your Name <tuffjamvisual@gmail.com>
2026-05-01 13:58:55 +0100

    Switch to Raleway font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Raleway, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 
-const geistSans = Geist({
+const geistSans = Raleway({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
