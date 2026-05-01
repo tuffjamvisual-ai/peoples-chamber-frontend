816906ea3940cba6fa0586200fdc68d77f7c09a5
Your Name <tuffjamvisual@gmail.com>
2026-05-01 23:54:30 +0100

    Fix middleware matcher to only apply to API routes

--- middleware.ts
+++ middleware.ts
@@ -45,5 +45,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|logo.png).*)'],
+  matcher: ['/api/:path*'],
 }
